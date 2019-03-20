fbdcbd1ff1ce886ab3c41ecd7733b34bd61df6b3
Yannick <yannick.francois@axiocode.com>
2019-03-20 16:17:22 +0100

    exercice 00

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ponyracer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ponyracer');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should have a title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ponyracer!');
+    const element = fixture.nativeElement;
+    expect(element.querySelector('h1').textContent)
+      .withContext('You should have an `h1` with the text Ponyracer')
+      .toContain('Ponyracer');
   });
 });
